900ee8c363a614f4ae10944d360404d5ff4f34ce
FerdiansyahDwi <ferdiansyahdwi00@gmail.com>
2024-11-22 12:58:09 +0700

    RecyclerView
    Update RecyclerView

--- ViewPager2Ferdi/app/src/main/java/com/example/viewpager2ferdi/FriendsFragment.kt
+++ ViewPager2Ferdi/app/src/main/java/com/example/viewpager2ferdi/FriendsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,9 @@ class FriendsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var friendAdapter: MyFriendAdapter
+    private val friends = mutableListOf<MyFriend>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +35,39 @@ class FriendsFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false)
+        val view = inflater.inflate(R.layout.fragment_friends, container, false)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        setupRecycleView()
+        return view
+    }
+
+    private fun setupRecycleView() {
+        friends.addAll(
+            listOf(
+                MyFriend("Ferdi","081432765987","ferdi@email.com"),
+                MyFriend("Maftuh", "081122334445","maftuh@email.com"),
+                MyFriend("Bintang", "085850123321","bintang@email.com"),
+                MyFriend("Aril", "085124424346","aril@email.com"),
+                MyFriend("Al", "085623115663","al@email.com"),
+                MyFriend("Kusuma", "08111225447","kusuma@email.com"),
+                MyFriend("Alfian", "081467394249","alfian@email.com"),
+                MyFriend("Dido", "085122369425","dido@email.com"),
+                MyFriend("Anas", "085212574421","anas@email.com"),
+                MyFriend("Wahyu", "085728391461","wahyu@email.com"),
+                MyFriend("Robi", "081953324146","robi@email.com"),
+                MyFriend("Naulan", "085127324189","naulan@email.com"),
+                MyFriend("Ainul", "081417344749","ainul@email.com"),
+                MyFriend("Axl", "082142536424","axl@email.com")
+            )
+        )
+
+        friendAdapter = MyFriendAdapter(friends)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = friendAdapter
     }
 
     companion object {
---
+++ ViewPager2Ferdi/app/src/main/java/com/example/viewpager2ferdi/MyFriend.kt
@@ -0,0 +1,7 @@
+package com.example.viewpager2ferdi
+
+class MyFriend (
+    val name: String,
+    val phone: String,
+    val email: String
+)
